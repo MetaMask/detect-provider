fc0aa50c0553b59670696b937c7baa270bc55ac0
Erik Marks <rekmarks@protonmail.com>
2020-09-08 14:57:03 -0700

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+import { MetaMaskInpageProvider } from '@metamask/inpage-provider'
+
+interface DetectEthereumProviderArgs {
+  mustBeMetaMask?: boolean,
+  silent?: boolean,
+  timeout?: number,
+}
+
+/**
+ * Returns a Promise that resolves to the value of window.ethereum if it is
+ * set within the given timeout, or null.
+ * The Promise will not reject, but an error will be thrown if invalid options
+ * are provided.
+ *
+ * @param {Object} [options] - Options bag.
+ * @param {boolean} [options.mustBeMetaMask] - Whether to only look for MetaMask
+ * providers. Default: false
+ * @param {boolean} [options.silent] - Whether to silence console errors. Does
+ * not affect thrown errors. Default: false
+ * @param {number} [options.timeout] - Milliseconds to wait for
+ * 'ethereum#initialized' to be dispatched. Default: 3000
+ * @returns {Promise<EthereumProvider | null>} A Promise that resolves with the
+ * Provider if it is detected within the given timeout, otherwise null.
+ */
+export interface detectEthereumProvider {
+  (options?: DetectEthereumProviderArgs): Promise<MetaMaskInpageProvider | null>;
+}
