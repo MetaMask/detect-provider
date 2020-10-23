c42e24b32cc1b64c2fd54a028ed8e10e2220bc4d
Erik Marks <rekmarks@protonmail.com>
2020-10-23 14:02:45 -0700

    Update build process for TypeScript

--- build.sh
+++ build.sh
@@ -9,5 +9,7 @@ BROWSERIFY_CMD="browserify -s detectEthereumProvider"
 mkdir -p ./dist
 rm -rf ./dist/*
 
-$BROWSERIFY_CMD src/index.js > dist/detect-provider.js
-$BROWSERIFY_CMD -p tinyify src/index.js > dist/detect-provider.min.js
+tsc --project . --outDir dist
+
+$BROWSERIFY_CMD -p esmify dist/index.js > dist/detect-provider.js
+$BROWSERIFY_CMD -p esmify -p tinyify dist/index.js > dist/detect-provider.min.js
